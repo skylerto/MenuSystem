682f72ba81371d861bb716eac75afd8388c51425
Skyler Layne <skylerclayne@gmail.com>
2016-06-09 18:12:28 -0400

    spec add left/right siblings

--- src/test/java/tree/MenuTreeTest.java
+++ src/test/java/tree/MenuTreeTest.java
@@ -25,33 +25,76 @@ public class MenuTreeTest {
     @Test(expected=IndexOutOfBoundsException.class)
     public void testIndexError()throws Exception {
         MenuItemBean bean = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
-        MenuItemBean one = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
-        MenuItemBean two = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
-        MenuItemBean three = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
         TreeNode<MenuItemBean> tree = new TreeNode<>(null);
 
         tree.addChild(bean);
         assertEquals(tree.hasRightSibling(), false);
         assertEquals(tree.hasLeftSibling(), false);
 
-
         TreeNode<MenuItemBean> root = tree.children.get(0);
         root.getLeftSibling();
+        root.getRightSibling();
 
-//
-//
-//        root.addChild(one);
-//        root.addChild(two);
-//        root.addChild(three);
-//
-//        TreeNode<MenuItemBean> current = root.children.get(0);
-//
-//        assertEquals(current.hasRightSibling(), true);
-//        assertEquals(current.hasLeftSibling(), false);
-//
-//        assertEquals(current.getRightSibling().data, two);
+    }
 
+    @Test
+    public void testHasRightSibling() throws Exception {
+        MenuItemBean bean = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        MenuItemBean one = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        MenuItemBean two = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        TreeNode<MenuItemBean> tree = new TreeNode<>(null);
 
+        tree.addChild(bean);
+        assertEquals(tree.hasRightSibling(), false);
+
+        TreeNode<MenuItemBean> root = tree.children.get(0);
+        root.addChild(one);
+        root.addChild(two);
+        TreeNode<MenuItemBean> current = root.children.get(0);
+        assertEquals(current.hasRightSibling(), true);
+    }
+
+    @Test
+    public void testHasLeftSibling() throws Exception {
+        MenuItemBean bean = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        MenuItemBean one = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        MenuItemBean two = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        TreeNode<MenuItemBean> tree = new TreeNode<>(null);
+        tree.addChild(bean);
+        assertEquals(tree.hasLeftSibling(), false);
+        TreeNode<MenuItemBean> root = tree.children.get(0);
+        root.addChild(one);
+        root.addChild(two);
+        TreeNode<MenuItemBean> current = root.children.get(1);
+        assertEquals(current.hasLeftSibling(), true);
+    }
+
+    @Test
+    public void testAddLeftSibling() throws Exception {
+        MenuItemBean bean = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        MenuItemBean one = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+
+        TreeNode<MenuItemBean> tree = new TreeNode<>(null);
+        tree.addChild(bean);
+
+        TreeNode<MenuItemBean> root = tree.children.get(0);
+        root.addLeftSibling(one);
+
+        assertEquals(root.getLeftSibling().data, one);
+    }
+
+    @Test
+    public void testAddRightSibling() throws Exception {
+        MenuItemBean bean = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        MenuItemBean one = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+
+        TreeNode<MenuItemBean> tree = new TreeNode<>(null);
+        tree.addChild(bean);
+
+        TreeNode<MenuItemBean> root = tree.children.get(0);
+        root.addRightSibling(one);
+
+        assertEquals(root.getRightSibling().data, one);
     }
 
     @Test
@@ -61,28 +104,17 @@ public class MenuTreeTest {
         MenuItemBean two = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
         MenuItemBean three = new MenuItemBean("first", "second", (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
         TreeNode<MenuItemBean> tree = new TreeNode<>(null);
-
         tree.addChild(bean);
         assertEquals(tree.hasRightSibling(), false);
         assertEquals(tree.hasLeftSibling(), false);
-
-
-
         TreeNode<MenuItemBean> root = tree.children.get(0);
         root.addChild(one);
         root.addChild(two);
         root.addChild(three);
-
         TreeNode<MenuItemBean> current = root.children.get(0);
-
         assertEquals(current.hasRightSibling(), true);
         assertEquals(current.hasLeftSibling(), false);
-
-//        assertEquals(current.getRightSibling().data, two);
-
         current = current.getRightSibling();
-
         assertEquals(current.getLeftSibling().data, one);
-
     }
 }
